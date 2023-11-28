ed36e7437c47fb11d33a98b8f510b73bafd0a91a
Alex <alex.oporto04@inacapmail.cl>
2023-11-28 20:01:01 -0300

    estilos registro

--- INVENTARIO/INVENTARIO/settings.py
+++ INVENTARIO/INVENTARIO/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myinventario'
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'myinventario',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
